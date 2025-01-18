ce97620f774d99c3859b41979a4054d77b0d4db2
SoutaGeuberth <soutadev@gmail.com>
2025-01-18 21:17:25 +0000

    Backend created

---
+++ Program.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", policy =>
+    {
+        policy.AllowAnyOrigin() // Permite cualquier origen
+              .AllowAnyMethod() // Permite cualquier método HTTP
+              .AllowAnyHeader(); // Permite cualquier encabezado
+    });
+}
+);
+builder.Services.AddControllers();
+
+
+var app = builder.Build();
+app.UseCors("AllowAllOrigins");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+var listaprendas = new List<Dictionary<string, string>>()
+{
+    new Dictionary<string, string>()
+    {
+        { "tittle", "camiseta" },
+        { "cost", "450" },
+        { "src", "https://http2.mlstatic.com/D_NQ_NP_805158-MLM75310632986_032024-O.webp" },
+        { "categoria", "camiseta" }
+    }
+};
+
+app.MapGet("/listaprendas", () =>
+{
+    return listaprendas;
+})
+.WithName("GetListaPrendas");
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
