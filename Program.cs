d6573f73fa87c11bb231abc82fd02c4b56b41cdc
SoutaGeuberth <soutadev@gmail.com>
2025-01-29 01:59:33 +0000

    Database connection to backend

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Collections.Generic;
+using MySqlConnector;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+
 builder.Services.AddCors(options =>
+
 {
     options.AddPolicy("AllowAllOrigins", policy =>
     {
@@ -15,9 +20,16 @@ builder.Services.AddCors(options =>
               .AllowAnyMethod() // Permite cualquier método HTTP
               .AllowAnyHeader(); // Permite cualquier encabezado
     });
+
 }
 );
 builder.Services.AddControllers();
+
+//Register the database
+builder.Services.AddMySqlDataSource(builder.Configuration.GetConnectionString("Default")!);
+
+
+
 
 
 var app = builder.Build();
@@ -31,10 +43,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
 var listaprendas = new List<Dictionary<string, string>>()
 {
@@ -47,25 +55,24 @@ var listaprendas = new List<Dictionary<string, string>>()
     }
 };
 
-app.MapGet("/listaprendas", () =>
+app.MapGet("/listaprendas", async (IConfiguration config) =>
 {
+    var connectionString = config.GetConnectionString("Default");
+    using var connection = new MySqlConnection(connectionString);
+
+    await connection.OpenAsync();
+
+    await using var command = new MySqlCommand("SELECT id,color FROM colors;", connection);
+    await using var reader = await command.ExecuteReaderAsync();
+    while (await reader.ReadAsync())
+    {
+        var value = reader.GetValue(1);
+        Console.WriteLine(value);
+    }
+
     return listaprendas;
 })
 .WithName("GetListaPrendas");
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
 
